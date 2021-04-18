f6f2e44de8076e227b06a550ef0b858dbd0a8a9f
Prasoon Sinha <sinhaprasoon@gmail.com>
2021-04-17 19:38:23 -0500

    Update test

--- sbrktest.c
+++ sbrktest.c
@@ -1,5 +1,8 @@
 #include "types.h"
 #include "user.h"
+#include "param.h"
+#include "stat.h"
+#include "ptentry.h"
 
 #define PGSIZE 4096
 
@@ -13,22 +16,36 @@
 
 int main(int argc, char *argv[])
 {
+    int num_entries = 15;
+    struct pt_entry *entries = malloc(sizeof(struct pt_entry) * num_entries);
+    getpgtable(entries, num_entries, 0);
+    for (int i = 0; i < num_entries; ++i)
+    {
+        printf(1, "%d: pdx: %x, ptx: %x, ppage: %x, present: %d, writable: %d, encrypted: %d, ref: %d\n", i, entries[i].pdx, entries[i].ptx, entries[i].ppage, entries[i].present, entries[i].writable, entries[i].encrypted, entries[i].ref);
+    }
+
     printf(1, "\nsbrk(PGSIZE)\n");
     sbrk(PGSIZE);
+    getpgtable(entries, num_entries, 0);
+    for (int i = 0; i < num_entries; ++i)
+    {
+        printf(1, "%d: pdx: %x, ptx: %x, ppage: %x, present: %d, writable: %d, encrypted: %d, ref: %d\n", i, entries[i].pdx, entries[i].ptx, entries[i].ppage, entries[i].present, entries[i].writable, entries[i].encrypted, entries[i].ref);
+    }
 
-    printf(1, "\nsbrk(PGSIZE / 2)\n");
-    sbrk(PGSIZE / 2);
+    // printf(1, "\nsbrk(PGSIZE / 2)\n");
+    // sbrk(PGSIZE / 2);
 
-    printf(1, "\nsbrk(PGSIZE)\n");
-    sbrk(PGSIZE);
+    // printf(1, "\nsbrk(PGSIZE)\n");
+    // sbrk(PGSIZE);
 
-    printf(1, "\nsbrk(1)\n");
-    sbrk(1);
+    // printf(1, "\nsbrk(1)\n");
+    // sbrk(1);
 
-    printf(1, "\nsbrk((PGSIZE / 2) - 1)\n");
-    sbrk((PGSIZE / 2) - 1);
+    // printf(1, "\nsbrk((PGSIZE / 2) - 1)\n");
+    // sbrk((PGSIZE / 2) - 1);
 
     printf(1, "\n");
+    free(entries);
     exit();
     return 0;
 }
