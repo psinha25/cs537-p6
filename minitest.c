c318823ce08f5d0b969293cf076c98e0764aa32d
Prasoon Sinha <sinhaprasoon@gmail.com>
2021-04-18 18:43:25 -0500

    Attempt to fix minitest

--- minitest.c
+++ minitest.c
@@ -9,11 +9,11 @@
 int main(int argc, char *argv[])
 {
     char *ptr = sbrk(PGSIZE); // Allocate one page
-    struct pt_entry pt_entry;
+    struct pt_entry *pt_entry = malloc(sizeof(struct pt_entry));
     // Get the page table information for newly allocated page
     // and the page should be encrypted at this point
-    getpgtable(&pt_entry, 1, 0);
+    getpgtable(pt_entry, 1, 0);
     ptr[0] = 0x0;
     // The page should now be decrypted and put into the clock queue
-    getpgtable(&pt_entry, 1, 1);
+    getpgtable(pt_entry, 1, 1);
 }
