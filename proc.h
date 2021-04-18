2e209bed0d7f91a9e25dd2104963d38c403be4a4
Prasoon Sinha <sinhaprasoon@gmail.com>
2021-04-18 11:46:19 -0500

    More changes

--- proc.h
+++ proc.h
@@ -60,7 +60,7 @@ struct proc
   struct file *ofile[NOFILE]; // Open files
   struct inode *cwd;          // Current directory
   char name[16];              // Process name (debugging)
-  struct clockqueue queue;    // Process's working set
+  struct clockqueue *queue;   // Process's working set
 };
 
 // Process memory is laid out contiguously, low addresses first:
